bec8ff1a7548cdf1f2bf8c833de2e559bea6938e
MacBook Air David <airm2@MacBook-Air-de-air.local>
2025-11-11 09:39:30 -0500

    Examen 11 nov

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Movie
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'year', 'director', 'genre')
+    search_fields = ('title', 'director', 'genre')
+    list_filter = ('year', 'genre')
--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField("Title", max_length=200)
+    genre = models.CharField("Genre", max_length=100)
+    director = models.CharField("Director", max_length=150)
+    year = models.IntegerField("Year")
+    synopsis = models.TextField("Synopsis", max_length=1000)
+
+    class Meta:
+        ordering = ['-year', 'title']
+        verbose_name = "Movie"
+        verbose_name_plural = "Movies"
+
+    def __str__(self):
+        return f"{self.title} ({self.year})"
